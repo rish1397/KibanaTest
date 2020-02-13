0a78b1d968ca6dd98a4a4f0f8be00c0cbfbb43f9
Rishabh Srivastava <srivasri@amazon.com>
2020-02-13 18:24:16 +0530

    Changes in tests for 1.4.0

--- src/integration-test/java/Tests.java
+++ src/integration-test/java/Tests.java
@@ -2,8 +2,12 @@ import java.io.File;
 import java.io.FileReader;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -21,12 +25,14 @@ public class Tests {
         String propertyFilePath = System.getProperty("driverPropertiesFilePath");
         System.out.println("property path is:"+propertyFilePath);
         Properties propertiesFile = new Properties();
+
         try {
             propertiesFile.load(new FileReader(propertyFilePath));
             System.out.println("BROWSER :"+propertiesFile.getProperty("BROWSER"));
             this.driver = initialiseBrowser(propertiesFile.getProperty("BROWSER"));
             System.out.println("Driver is:"+this.driver);
             this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            this.driver.manage().window().setSize(new Dimension(2500, 1500));
             actions = new Actions(this.driver);
             assertPage = new AssertPage(this.driver);
         } catch (Exception e) {
@@ -45,13 +51,31 @@ public class Tests {
     public WebDriver initialiseBrowser(String browser) {
         switch (browser) {
             case "FIREFOX":
-                System.setProperty("webdriver.gecko.driver",System.getProperty("integTestResourcesDir")+"\\geckodriver.exe");
+                FirefoxOptions options = new FirefoxOptions();
+                //options.setHeadless(true);
+                setFirefoxDriverPath();
                 System.out.println("geckodriver path is:"+System.getProperty("webdriver.gecko.driver"));
                 System.out.println("switch returned FIREFOX DRIVER");
-                return new FirefoxDriver();
+                return new FirefoxDriver(options);
         }
         System.out.println("switch returned NULL");
         return null;
+    }
+
+    private void setFirefoxDriverPath(){
+        String osName = System.getProperty("os.name").toLowerCase();
+        try {
+            if (osName.matches(".*mac.*")) {
+                System.setProperty("webdriver.gecko.driver", System.getProperty("integTestResourcesDir") + "/geckodriver_mac");
+            } else if (osName.matches(".*win.*")) {
+                System.setProperty("webdriver.gecko.driver", System.getProperty("integTestResourcesDir") + "/geckodriver.exe");
+            } else {
+                System.setProperty("webdriver.gecko.driver", System.getProperty("integTestResourcesDir") + "/geckodriver");
+            }
+        }
+        catch(Exception e){
+            System.out.println("geckodriver path not set"+e);
+        }
     }
 
     @Test(enabled = true, priority = 0)
@@ -72,7 +96,7 @@ public class Tests {
 
     @Test(enabled = true, priority = 1)
     public void loginTest() {
-        Assert.assertTrue(assertPage.assertPageByTitle("Kibana"), "Page Not Found");
+         Assert.assertTrue(assertPage.assertPageByTitle("Kibana"), "Page Not Found");
         //load the property file to take the input
         String loginPropertiesFilePath = System.getProperty("loginPropertiesFilePath");
         //fill the input to the fields
@@ -82,7 +106,7 @@ public class Tests {
         //click the login button
         actions.click(loginbuttonID, "ID");
         //Verify the home page by verifying an element(identified by locator[NAME, ID, XPATH] and its value[location])
-        Assert.assertTrue(assertPage.assertPageByElement("//*[@id=\"kibana-app\"]/div[4]/div/header/div/h1", "XPATH"), "Page Not Found");
+        assertPage.assertPageByElement("//*[@id=\"kibana-app\"]/div[4]/div/header/div/h1", "XPATH");
     }
 
     @Test(enabled = true, priority = 3, dataProvider = "clickOption-data-provider", dataProviderClass = DataProvider.class)
@@ -90,10 +114,10 @@ public class Tests {
         actions.click(location, locator);
     }
 
-    @Test(enabled = true, priority = 4, dataProvider = "verifyPanelElement-data-provider", dataProviderClass = DataProvider.class)
-    public void verifyPanelElement(String iconLocation, String iconLocatorType, String expectedElementLocation, String expectedElementLocatorType, String description) {
-        actions.click(iconLocation, iconLocatorType);
-        Assert.assertTrue(assertPage.assertPageByElement(expectedElementLocation, expectedElementLocatorType), "Page Not Found:"+description);
+    @Test(enabled = true, priority = 4, dataProvider = "verifyPanelElement-data-provider-2", dataProviderClass = DataProvider.class)
+    public void verifyPanelElement(By iconLocator, By expexctedElementLocator, String description) {
+        actions.click(iconLocator);
+        assertPage.assertPageByElement(expexctedElementLocator);
     }
 
 }
